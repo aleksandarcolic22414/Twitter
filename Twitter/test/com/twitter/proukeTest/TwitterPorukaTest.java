12fbb039af5a9f34b9e74233076507b4f3456ee4
aleksandarcolic22414 <acacolic@gmail.com>
2016-04-07 17:46:56 +0200

    Napravljeni su novi paketi
    Napravljeni su novi paketi com.twitterTestiranje i
    com.twitter.porukeTestiranje u okviru src folera test i prebacene su
    odgovarajuce klase za testiranje u te pakete.

--- Twitter/test/com/twitter/proukeTest/TwitterPorukaTest.java
+++ Twitter/test/com/twitter/proukeTest/TwitterPorukaTest.java
@@ -1,4 +1,4 @@
-package junitTestiranje;
+package com.twitter.proukeTest;
 
 import static org.junit.Assert.*;
 
--- Twitter/test/com/twitterTest/TwitterTest.java
+++ Twitter/test/com/twitterTest/TwitterTest.java
@@ -1,4 +1,4 @@
-package junitTestiranje;
+package com.twitterTest;
 
 import static org.junit.Assert.*;
 
